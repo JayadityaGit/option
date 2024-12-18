17bb895a61e14f44ec1ad86a5cce4667c734bc02
Jayaditya Gedhella <jgedela@gitam.in>
2024-12-18 17:53:17 +0530

    can reload movie page

--- src/Pages/MovieStream.tsx
+++ src/Pages/MovieStream.tsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router"
 
 
 const MovieStream = () => {
-
   const location = useLocation();
-  const id = location.state.tmdbId;
+  const [tmdbdId, setTmdbId] = useState(location.state.tmdbId);
+
+  useEffect(() => {
+    
+    const id: string = location.state.tmdbId;
+    setTmdbId(id)
+
+  }, [])
+  
+
+
 
   return (
     <div className="w-full max-w-7xl mx-auto px-4 py-8">
       <div className="relative w-full aspect-video">
         <iframe
-          src={`https://vidlink.pro/movie/${id}`}
+          src={`https://vidlink.pro/movie/${tmdbdId}`}
           allowFullScreen
           className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
         ></iframe>
