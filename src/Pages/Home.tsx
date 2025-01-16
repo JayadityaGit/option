9cd7398a7574783be7da87435bd8f10f92ec88c9
JAYADITYA <96861162+JayadityaGit@users.noreply.github.com>
2025-01-16 15:52:11 +0530

    Update Home.tsx

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -78,7 +78,7 @@ const Home = () => {
     <div className="flex justify-center p-5 md:p-0">
       <PinContainer
         title="whatever happens ... happens, love u :)"
-        href="https://www.youtube.com/watch?v=wDqArJu1Rbs&t=762s"
+        href="https://www.youtube.com/watch?v=2B_odZRtgf8"
       >
         <div className="flex flex-col items-center justify-center w-[15rem] md:w-[20rem]  h-auto">
           <img
