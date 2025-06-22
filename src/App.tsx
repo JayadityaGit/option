2deac0cedd089e5942d5bb922005db2a0d5896db
JAYADITYA <96861162+JayadityaGit@users.noreply.github.com>
2025-06-22 18:05:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router"
 import Home from "./Pages/Home"
 import TvStream from "./Pages/TvStream"
@@ -17,42 +16,48 @@ import {
 } from "@/components/ui/alert-dialog"
 import { Button } from "./components/ui/button"
 
-
-
 const App = () => {
   return (
-    
     <Router>
-      <div className="flex justify-center p-10 space-x-3">
-      <ModeToggle/>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button>premium</Button></AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Remove Ads</AlertDialogTitle>
-            <AlertDialogDescription>
-           Install the <strong>uBlock Origin</strong> extension on your browser to block intrusive ads and improve your browsing experience.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction><a  target="blank" href="https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en">install for chrome</a></AlertDialogAction>
-            <AlertDialogAction><a  target="blank" href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/">install for firefox</a></AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-
+      <div className="flex justify-end p-10 space-x-3">
+        <ModeToggle />
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button>premium</Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Remove Ads</AlertDialogTitle>
+              <AlertDialogDescription>
+                Install the <strong>uBlock Origin</strong> extension on your browser to block intrusive ads and improve
+                your browsing experience.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>
+                <a
+                  target="blank"
+                  href="https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en"
+                >
+                  install for chrome
+                </a>
+              </AlertDialogAction>
+              <AlertDialogAction>
+                <a target="blank" href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/">
+                  install for firefox
+                </a>
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
       <Routes>
-      
-        <Route path="/" element={<Home/>}/>
-         <Route path="/tv" element={<TvStream/>}/>
-         <Route path="/movie" element={<MovieStream/>}/>
-        <Route/>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<TvStream />} />
+        <Route path="/movie" element={<MovieStream />} />
       </Routes>
     </Router>
-
   )
 }
 
